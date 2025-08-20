7f8e4417ec47c8165dd9450bd4de647668cdd8b8
irpanzy <irfanoputra27@gmail.com>
2025-08-20 09:40:06 +0700

    chore(api): update routes to use versioned prefixes

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,10 @@ async function init() {
     const { PORT } = process.env;
 
     app.use(bodyParser.json());
-    app.use("/api/v1", router);
+    app.use("/api", router);
+    app.get("/", (req, res) => {
+      res.send("Server is running");
+    });
 
     app.listen(PORT, () => {
       console.log(`Server is running on port http://localhost:${PORT}/api/v1`);
